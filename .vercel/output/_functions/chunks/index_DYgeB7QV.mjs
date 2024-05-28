52c809c2967c569d91bb48185f86c94dbb1ca9dd
Jose Delgado <j3leonardodelgado@gmail.com>
2024-05-28 00:14:23 -0400

    build fixed

---
+++ .vercel/output/_functions/chunks/index_DYgeB7QV.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_CYBhGQUk.mjs');
+
+export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), react()],
-  scripts: [{ src: '/path/to/forma.js', type: 'module' }],
-  output: 'hybrid',
-  site: 'https://porfolio.dev/'
+    integrations: [tailwind(), robotsTxt()],
 });
