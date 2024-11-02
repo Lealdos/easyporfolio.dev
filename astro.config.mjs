da6d60c596223282170171c5e6bb1d272637fbea
Jose Delgado <j3leonardodelgado@gmail.com>
2024-11-02 16:13:22 -0400

    delete vercel folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), robotsTxt()],
-
     site: 'https://Lealdos.github.io',
-    base: 'easyporfolio.dev',
+    output: 'server',
+    adapter: vercel(),
 });
