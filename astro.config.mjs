63ed15e49ebef1f452d303b6b0ecb87b97ec5fa2
Jose Delgado <j3leonardodelgado@gmail.com>
2024-11-02 16:15:19 -0400

    Replace Vercel adapter with Netlify in Astro config for deployment adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), robotsTxt()],
     site: 'https://Lealdos.github.io',
     output: 'server',
-    adapter: vercel(),
+    adapter: netlify(),
 });
