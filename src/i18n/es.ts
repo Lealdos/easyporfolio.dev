8a1a48b9afdff00e2e31eda1ca0f07d8c509387d
Jose Delgado <j3leonardodelgado@gmail.com>
2025-09-25 17:47:08 -0400

    fix: typo

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -1,35 +1,35 @@
-import type { i18nUI } from "./i18n.type";
+import type { i18nUI } from './i18n.type';
 
-export const es : i18nUI = {
-    languageLabel: "Idioma",
+export const es: i18nUI = {
+    languageLabel: 'Idioma',
     languageOptions: {
-        es: "Español",
-        en: "Inglés"
+        es: 'Español',
+        en: 'Inglés',
     },
     pageInfo: {
-        'title': 'Portafolio Jose Delgado - Ingeniero de software - Desarrollador web',
-        'description':'Contrata a Jose Delgado para crear, mejorar o mantener tu aplicacion web o servicios. Desarrollador web especializado en crear apliciones unicas. '
-      },
-      sections:{
-        'project':'Proyectos',
-        'workHistory': 'Experiencia laboral',
-        'aboutMe': 'Acerca de mi',
-      },
-      nav :{
-        'project': 'Proyectos',
-        'about': 'Acerca de',
-        'experience': 'Experiencia',
-        'contact': 'Contacto',
-        'language': 'seleciona tu idioma',
-        'theme': 'elige el modo de pantalla',
-      
-      },
-      hero:{
-        'jobStatus':'Open to work',
-        'contactBadge':'Contact me',
-      },
-      projects:{
-        'CodeBadge': 'Code',
-        'LinkBadge': 'Link',
-    }
-} 
+        title: 'Portafolio Jose Delgado - Ingeniero de software - Desarrollador web',
+        description:
+            'Contrata a Jose Delgado para crear, mejorar o mantener tu aplicación web o servicios. Desarrollador web especializado en crear aplicaciones únicas. ',
+    },
+    sections: {
+        project: 'Proyectos',
+        workHistory: 'Experiencia laboral',
+        aboutMe: 'Acerca de mi',
+    },
+    nav: {
+        project: 'Proyectos',
+        about: 'Acerca de',
+        experience: 'Experiencia',
+        contact: 'Contacto',
+        language: 'selecciona tu idioma',
+        theme: 'elige el modo de pantalla',
+    },
+    hero: {
+        jobStatus: 'Open to work',
+        contactBadge: 'Contact me',
+    },
+    projects: {
+        CodeBadge: 'Code',
+        LinkBadge: 'Link',
+    },
+};
