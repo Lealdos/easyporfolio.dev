09ff6003a132f1d46568b55e6901edbbf140ecc2
Jose Delgado <j3leonardodelgado@gmail.com>
2025-09-26 01:43:18 -0400

    fix: correct formatting and complete i18nUI type definition

--- src/i18n/i18n.type.ts
+++ src/i18n/i18n.type.ts
@@ -1,11 +1,11 @@
-import type { Languages } from "./Language";
+import type { Languages } from './Language';
 
-export type i18nUI  = {
+export type i18nUI = {
     languageLabel: string;
     languageOptions: Record<Languages, string>;
     pageInfo: {
         title: string;
-        description:string;
+        description: string;
     };
     sections: {
         project: string;
@@ -23,9 +23,43 @@ export type i18nUI  = {
     hero: {
         jobStatus: string;
         contactBadge: string;
+        title: string;
+        description: string;
     };
     projects: {
+        projects: {
+            name: string;
+            description: string;
+            techStack: string[];
+            image: string;
+            isActive: boolean;
+            link?: string;
+            github?: string;
+        }[];
         CodeBadge: string;
         LinkBadge: string;
     };
-}
+    experiences: {
+        experience: {
+            title: string;
+            company: string;
+            description: string;
+            date: Date | string;
+            endDate: string | null;
+        }[];
+    };
+    aboutMe: {
+        paragraph: string[];
+    };
+    themeToggle: {
+        light: string;
+        dark: string;
+        system: string;
+    };
+    footer: {
+        name: string;
+        contact: string;
+        text: string;
+        about: string;
+    };
+};
