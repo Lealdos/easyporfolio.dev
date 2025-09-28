f0bac79473c1691d2f900c7ab2cb0ef83448635f
Jose Delgado <j3leonardodelgado@gmail.com>
2025-09-28 12:12:26 -0400

    testing on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), robotsTxt()],
-    output: 'server',
-    adapter: netlify(),
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es'],
-        routing: {
-            prefixDefaultLocale: true,
-        },
-    },
+  integrations: [tailwind(), robotsTxt()],
+  output: 'server',
+
+  i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'es'],
+      routing: {
+          prefixDefaultLocale: true,
+      },
+  },
+
+  adapter: vercel(),
 });
