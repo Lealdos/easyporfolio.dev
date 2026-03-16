dbac421dd9d9f51fad997e81b8ec3b7739f16a71
Jose Delgado <j3leonardodelgado@gmail.com>
2026-03-16 19:09:13 -0400

    refactor: switch adapter from Netlify to Vercel and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), robotsTxt()],
     output: 'server',
-    adapter: netlify(),
+    adapter: vercel(),
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'es'],
